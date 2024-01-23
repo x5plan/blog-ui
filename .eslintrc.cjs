0d06296fcd6c791ccb8879c02505af1ee7a03d42
Antares <antares.mi@outlook.com>
2024-01-23 16:07:01 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,73 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        project: "tsconfig.json",
+    },
+    ignorePatterns: [".eslintrc.cjs", "vite.config.ts"],
+    plugins: ["@typescript-eslint/eslint-plugin", "simple-import-sort", "react-hooks"],
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "plugin:react/recommended",
+    ],
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        "arrow-parens": ["error", "always"],
+        curly: ["error", "multi-line"],
+        "import/no-cycle": "off",
+        "no-extend-native": "error",
+        "no-unused-vars": "off",
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error",
+        "simple-import-sort/exports": "error",
+        "simple-import-sort/imports": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                selector: "interface",
+                format: ["PascalCase"],
+                prefix: ["I"],
+            },
+            {
+                selector: "enum",
+                format: ["PascalCase"],
+                prefix: ["CE_", "E_"],
+            },
+            {
+                selector: ["function", "classMethod"],
+                modifiers: ["async"],
+                format: ["camelCase", "PascalCase"],
+                suffix: ["Async"],
+            },
+        ],
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/no-duplicate-enum-values": "error",
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                vars: "local",
+                args: "after-used",
+            },
+        ],
+        "@typescript-eslint/prefer-as-const": "error",
+    },
+};
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <div />
+    </React.StrictMode>,
+);
