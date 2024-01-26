ebf10b7b7feb66e648e3be6bdf9b77636c098b7a
Zhehao Mi <zhehaomi@microsoft.com>
2024-01-26 14:22:15 +0800

    app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,30 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <div />
-    </React.StrictMode>,
-);
+import { GlobalErrorBoundary } from "./Common/Components/GlobalErrorBoundary";
+import { initEnvAction } from "./Features/Environment/Actions";
+import { store } from "./Features/Store/Store";
+
+const AppLazy = React.lazy(() => import("./App"));
+
+function render() {
+    createRoot(document.getElementById("root")).render(
+        <React.StrictMode>
+            <GlobalErrorBoundary>
+                <Provider store={store}>
+                    <React.Suspense fallback={null}>
+                        <AppLazy />
+                    </React.Suspense>
+                </Provider>
+            </GlobalErrorBoundary>
+        </React.StrictMode>,
+    );
+}
+
+function launch() {
+    store.dispatch(initEnvAction);
+    render();
+}
+
+launch();
