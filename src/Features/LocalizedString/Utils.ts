795a80001d16b37cd64917f3aa6ac66c7ab64043
Antares <antares.mi@outlook.com>
2024-02-03 02:01:09 +0800

    Add localized string script (#4)
    * update packages
    
    * string scripts
    
    * update config
    
    * fix
    
    * fix
    
    * remove ts-node
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * lint code
    
    * fix gitignore

---
+++ src/Features/LocalizedString/Utils.ts
@@ -0,0 +1,50 @@
+import type { CE_Language } from "./Locales";
+import { defaultLanguage, rtlLanguages, supportedLanguages } from "./Locales";
+import type { CE_Locale } from "./Strings";
+
+export function getIsRtlLanguage(lang: CE_Language) {
+    return rtlLanguages.findIndex((l) => lang.startsWith(l)) >= 0;
+}
+
+export function selectLanguage(languages: string | string[]): CE_Language {
+    if (!languages) return null;
+
+    if (typeof languages === "string") languages = [languages];
+    languages = languages.map((lang) => lang.trim().toLowerCase());
+
+    // Strict match
+    for (const language of languages) {
+        const lang = supportedLanguages.find((value) => value === language);
+        if (lang) return lang;
+    }
+
+    // Match prefix
+    for (const language of languages) {
+        const lang = supportedLanguages.find((value) => value === language.split("-")[0]);
+        if (lang) return lang;
+    }
+
+    return null;
+}
+
+export function getNavigatorLanguage() {
+    return selectLanguage(Array.from(navigator?.languages));
+}
+
+export function getPreferLanguage() {
+    return getNavigatorLanguage() || defaultLanguage;
+}
+
+export async function loadLocaleAsync(lang: CE_Language) {
+    const module = await import(`../../assets/locales/strings.${lang}.js`);
+    return module.default as ILocalizedStrings;
+}
+
+export function getLocalizedString(strings: ILocalizedStrings, id: CE_Locale) {
+    if (strings[id]) {
+        return strings[id];
+    } else {
+        // TODO: log error here
+        return "BAD STRING";
+    }
+}
