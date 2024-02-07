dde1355547e9271a2fc91183897e5771c78475b8
Antares <antares.mi@outlook.com>
2024-02-08 00:48:34 +0800

    page feature

---
+++ src/Features/Page/Actions.ts
@@ -0,0 +1,18 @@
+import { createAction } from "@reduxjs/toolkit";
+
+import type { IAppDispatch } from "../Store/Types";
+import type { CE_PageType, IPageState } from "./Types";
+
+const UPDATE_PAGE = "Page/Update";
+
+export const setPage = createAction(UPDATE_PAGE, (props: Partial<IPageState>) => ({
+    payload: props,
+}));
+
+export const updatePageTypeAction = (pageType: CE_PageType) => (dispatch: IAppDispatch) => {
+    dispatch(setPage({ pageType }));
+};
+
+export const updatePageTitleAction = (pageTitle: string) => (dispatch: IAppDispatch) => {
+    dispatch(setPage({ pageTitle }));
+};
--- src/Features/Store/Reducers.ts
+++ src/Features/Store/Reducers.ts
@@ -2,11 +2,13 @@ import type { ReducersMapObject } from "redux";
 
 import { envReducer } from "../Environment/Reducer";
 import { localeReducers } from "../LocalizedString/Reducer";
+import { pageReducer } from "../Page/Reducer";
 import type { IReducerMap, IStaticReducerMap } from "./Types";
 
 export const staticReducers: ReducersMapObject<IStaticReducerMap> = {
     env: envReducer,
     locale: localeReducers,
+    page: pageReducer,
 };
 
 export const reducers: ReducersMapObject<IReducerMap> =
