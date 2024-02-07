80188266c524206d48dae889f8bcbf2162dc2581
Antares <antares.mi@outlook.com>
2024-02-07 17:20:14 +0800

    fix localized string hook type

--- src/Features/LocalizedString/Hooks.ts
+++ src/Features/LocalizedString/Hooks.ts
@@ -8,17 +8,19 @@ import { getLocalizedString } from "./Utils";
 
 export const useIsRtl = () => useAppSelector(getIsRtl);
 
-export const useLocalizedString = (key: CE_Strings) => {
+export const useLocalizedString = <T extends CE_Strings>(key: T) => {
     const strings = useAppSelector(getStrings);
 
-    return React.useMemo(() => getLocalizedString(strings, key), [strings, key]);
+    return React.useMemo(() => getLocalizedString<T>(strings, key), [strings, key]);
 };
 
-export const useLocalizedStrings = (...keys: CE_Strings[]) => {
+export const useLocalizedStrings = <T extends CE_Strings[]>(...keys: T) => {
     const strings = useAppSelector(getStrings);
 
     return React.useMemo(
         () => keys.map((key) => getLocalizedString(strings, key)),
         [strings, keys],
-    );
+    ) as unknown as {
+        [K in keyof T]: ReturnType<typeof getLocalizedString<T[K]>>;
+    };
 };
