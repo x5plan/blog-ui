8074acb7916a9f1345c4d2a9a54b382f7f374770
Antares <antares.mi@outlook.com>
2024-02-09 22:44:07 +0800

    remove unused functions

--- src/Features/Auth/Actions.ts
+++ src/Features/Auth/Actions.ts
@@ -1,7 +1,6 @@
 import { createAction } from "@reduxjs/toolkit";
 
 import { CE_StroageKeys } from "@/Common/Enums/StorageKeys";
-import type { IUserBaseDetail } from "@/Common/ServerTypes/User";
 import { getLocalStorage } from "@/Common/Utilities/SafeStorage";
 
 import type { IAppDispatch } from "../Store/Types";
@@ -16,15 +15,4 @@ export const setAuthAction = createAction(UPDATE_AUTH, (props: Partial<IAuthStat
 export const updateBearerToken = (token: string) => (dispatch: IAppDispatch) => {
     dispatch(setAuthAction({ bearerToken: token }));
     getLocalStorage().setItem(CE_StroageKeys.ApiBearerToken, token);
-};
-
-export const signInUserAction =
-    (token: string, user: IUserBaseDetail) => (dispatch: IAppDispatch) => {
-        dispatch(updateBearerToken(token));
-        dispatch(setAuthAction({ currentUser: user }));
-    };
-
-export const signOutUserAction = () => (dispatch: IAppDispatch) => {
-    dispatch(updateBearerToken(""));
-    dispatch(setAuthAction({ currentUser: null }));
 };
