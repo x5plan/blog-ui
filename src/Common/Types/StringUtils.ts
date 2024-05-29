4f4749e82469ea41e7a349b4fb8dc44c45ef58cf
Antares <antares.mi@outlook.com>
2024-05-29 15:06:08 +0800

    fix

--- src/Common/Types/StringUtils.ts
+++
@@ -1,7 +0,0 @@
-export type StartWith<P extends string> = `${P}${string}`;
-export type NotStartWith<T extends string, P extends string> = T extends `${P}${string}`
-    ? never
-    : T;
-
-export type EndWith<P extends string> = `${string}${P}`;
-export type NotEndWith<T extends string, P extends string> = T extends `${string}${P}` ? never : T;
